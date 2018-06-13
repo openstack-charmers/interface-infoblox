abd0b6bfade8866c03af35510c578bc5a74c87ba
Michael Skalka <michael.skalka@canonical.com>
2018-06-13 11:36:35 -0400

    changed name to infoblox for ease of use

--- provides.py
+++ provides.py
@@ -11,15 +11,22 @@ class Infoblox(RelationBase):
 
     scope = scopes.GLOBAL
 
-    @hook('{provides:infoblox-api}-relation-{joined, changed}')
+    @hook('{provides:infoblox}-relation-joined')
     def peers_joined(self):
         conv = self.conversation()
         conv.set_state('{relation_name}.joined')
         self.set_state('{relation_name}.connected')
         self.set_state('{relation_name}.available')
 
+    @hook('{provides:infoblox}-relation-changed')
+    def peers_joined(self):
+        conv = self.conversation()
+        if conv.get_remote('configured'):
+            conv.set_state('{relation_name}.configured')
+        else:
+            conv.remove_state('{relation_name}.configured')
 
-    @hook('{provides:infoblox-api}-relation-{broken, departed}')
+    @hook('{provides:infoblox}-relation-{broken, departed}')
     def peers_departed(self):
         conv = self.conversation()
         conv.remove_state('{relation_name}.joined')
