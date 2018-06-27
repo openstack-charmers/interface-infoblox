8c3bc0841753142d02f783837d893c5e71ca9558
Michael Skalka <michael.skalka@canonical.com>
2018-06-27 14:29:16 -0500

    tweaked to make the functions more generic

--- provides.py
+++ provides.py
@@ -12,14 +12,14 @@ class Infoblox(RelationBase):
     scope = scopes.GLOBAL
 
     @hook('{provides:infoblox}-relation-joined')
-    def infoblox_neutron_joined(self):
+    def infoblox_joined(self):
         conv = self.conversation()
         conv.set_state('{relation_name}.joined')
         self.set_state('{relation_name}.connected')
         self.set_state('{relation_name}.available')
 
     @hook('{provides:infoblox}-relation-changed')
-    def infoblox_neutron_changed(self):
+    def infoblox_changed(self):
         conv = self.conversation()
         conv.remove_state('{relation_name}.configured')
         if conv.get_remote('configured'):
@@ -28,26 +28,26 @@ class Infoblox(RelationBase):
             conv.set_state('infoblox.create-defs')
 
     @hook('{provides:infoblox}-relation-{broken, departed}')
-    def infoblox_neutron_departed(self):
+    def infoblox_departed(self):
         conv = self.conversation()
         conv.remove_state('{relation_name}.joined')
         conv.set_state('{relation_name}.departing')
         self.remove_state('{relation_name}.available')
         self.remove_state('{relation_name}.connected')
 
-    def configure_plugin(self, dc_id=None, config=None):
+    def configure_principal(self, config, dc_id=None):
         """Send principle infoblox connection information"""
-        conversation = self.conversation()
+        conv = self.conversation()
         relation_info = {
             'dc_id': dc_id,
             'config': json.dumps(config),
         }
-        conversation.set_remote(**relation_info)
+        conv.set_remote(**relation_info)
 
     def migrate_principal(self, dc_id=None, config=None):
         """Request a restart of principle services"""
-        conversation = self.conversation()
+        conv = self.conversation()
         relation_info = {
             'migrate': True
         }
-        conversation.set_remote(**relation_info)
+        conv.set_remote(**relation_info)
