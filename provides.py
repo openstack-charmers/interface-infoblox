82f302b807ce4e23ae50f99cde7a38cba9769dfa
Aymen Frikha <aymen.frikha@canonical.com>
2019-03-31 16:21:48 -0400

    replace relationBase class by Endpoint class

--- provides.py
+++ provides.py
@@ -1,39 +1,35 @@
-#!/usr/bin/python
-
-import json
-
-from charms.reactive import RelationBase
-from charms.reactive import hook
+from charms.reactive import Endpoint
+from charms.reactive import set_flag, clear_flag
+from charms.reactive import when, when_all, when_not
 from charms.reactive import scopes
 
 
-class InfobloxProvides(RelationBase):
-
+class InfobloxProvides(Endpoint):
     scope = scopes.GLOBAL
 
-    @hook('{provides:infoblox}-relation-joined')
-    def infoblox_joined(self):
-        conv = self.conversation()
-        conv.set_state('{relation_name}.joined')
-        self.set_state('{relation_name}.connected')
-        self.set_state('{relation_name}.available')
 
-    @hook('{provides:infoblox}-relation-changed')
-    def infoblox_changed(self):
-        conv = self.conversation()
-        conv.remove_state('{relation_name}.configured')
-        if conv.get_remote('create-defs'):
-            conv.set_state('infoblox.create-defs')
+    @when_all('endpoint.{endpoint_name}.changed',
+              'endpoint.{endpoint_name}.joined')
+    @when_not('endpoint.{endpoint_name}.changed.neutron_api_ready')
+    def joined(self):
+        set_flag(self.expand_name('endpoint.{endpoint_name}.connected'))
+        clear_flag(self.expand_name('endpoint.{endpoint_name}.configured'))
+        clear_flag(self.expand_name('endpoint.{endpoint_name}.changed'))
 
-    @hook('{provides:infoblox}-relation-{broken, departed}')
-    def infoblox_departed(self):
-        conv = self.conversation()
-        conv.remove_state('{relation_name}.joined')
-        conv.set_state('{relation_name}.departing')
-        self.remove_state('{relation_name}.available')
-        self.remove_state('{relation_name}.connected')
+    @when_not('endpoint.{endpoint_name}.joined')
+    def broken(self):
+        clear_flag(self.expand_name('endpoint.{endpoint_name}.connected'))
+        clear_flag(self.expand_name('endpoint.{endpoint_name}.departed'))
 
-    def configure_principal(self, relation_info):
-        """Send principle infoblox connection information"""
-        conv = self.conversation()
-        conv.set_remote(**relation_info)
+    @when('endpoint.{endpoint_name}.changed.neutron_api_ready')
+    def neutron_ready(self):
+        clear_flag(
+            self.expand_name(
+                'endpoint.{endpoint_name}.changed.neutron_api_ready'))
+        set_flag(self.expand_name('endpoint.{endpoint_name}.neutron_server_ready'))
+
+    def configure_principal(self, configuration):
+        """Send principle infoblox configuration"""
+
+        for relation in self.relations:
+            relation.to_publish.update(configuration)
